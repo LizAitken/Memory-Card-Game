a82673dc537021966508ca4d2f170cf4f22f99a1
Liz Aitken <liz.a.aitken@gmail.com>
2019-06-25 15:17:12 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const headerStyle = {
+  background: 'black',
+  color: 'white'
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={headerStyle}>
+        <title>Memory Game</title>
+        <h2 class="title-name">Memory Game</h2>
+        <p class="sub-title">Match cards to win</p>
       </header>
     </div>
   );
